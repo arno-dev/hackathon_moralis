b82a7197f61cbb99feae4ce85ac6efbfce443b96
Arnaud <arnaud@agilityfeat.com>
2022-09-06 19:39:27 +0700

    refactor: the unit testing to display an uniform flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@ import 'package:hackathon_moralis/utilities/eth.dart';
 import 'package:hackathon_moralis/utilities/file_handler.dart';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pinenacl/api/authenticated_encryption.dart';
 
 void main() {
   test("Get credential by bip32", () async {
@@ -16,51 +17,109 @@ void main() {
     expect(actual, walletCredential.address);
   });
 
-  group("Encryption of IPFS", () {
-    AsymmetricEncryption asymmetricEncryption = AsymmetricEncryption();
-    const walletPrivateKey = "alkdjfj43iojklflkasdjlfksj";
-    String encrypted = "";
-    const message =
-        "There is no conspiracy out there, but lack of the incentives to drive the people towards the answers.";
+  group("Entire flow of sharing encrypted IPFS file and decryption", () {
+    late AsymmetricEncryption asymmetricEncryption;
+    late String mnemonic1;
+    late String mnemonic2;
+    late String mnemonic3;
+    final eth = Eth();
+    late WalletCredential walletCredential1;
+    late WalletCredential walletCredential2;
+    late WalletCredential walletCredential3 = eth.getCredential(mnemonic3);
+    late PrivateKey skWallet1;
+    late PrivateKey skWallet2;
+    late PrivateKey skWallet3;
+    late FileHandler fileHandler;
 
-    // Generate Bob's private key, which must be kept secret
-    final skbob = asymmetricEncryption
-        .generatePrivateKeyFromWalletPrivate(walletPrivateKey);
-    // Bob's public key can be given to anyone wishing to send
-    // Bob an encrypted message
-    final pkbob = skbob.publicKey;
-    // Alice does the same and then Alice and Bob exchange public keys
-    final skalice = asymmetricEncryption.generatePrivateKey();
-    final pkalice = skalice.publicKey;
-    test("Encrypt IPFS key by pinenacl", () {
-      String bobShareKey =
-          asymmetricEncryption.encryptData(message, skbob, pkalice);
-      encrypted = bobShareKey;
-      expect(encrypted, bobShareKey);
+    setUpAll(() {
+      asymmetricEncryption = AsymmetricEncryption();
+      // Generate two wallet
+      mnemonic1 =
+          "toward paper enemy brother man achieve coconut dad tent amateur advance copper";
+      mnemonic2 =
+          "copper paper enemy brother man achieve coconut dad tent amateur advance toward";
+      mnemonic3 =
+          "copper paper enemy man brother achieve coconut dad tent amateur advance toward";
+      walletCredential1 = eth.getCredential(mnemonic1);
+      walletCredential2 = eth.getCredential(mnemonic2);
+      walletCredential3 = eth.getCredential(mnemonic3);
+
+      // Generate a private key as it will be used as IPFS key from Wallet 1
+      skWallet1 = asymmetricEncryption
+          .generatePrivateKeyFromWalletPrivate(walletCredential1.privateKey);
+
+      // Generate a private key as it will be used as IPFS key from Wallet 2
+      skWallet2 = asymmetricEncryption
+          .generatePrivateKeyFromWalletPrivate(walletCredential2.privateKey);
+
+      // Generate a private key as it will be used as IPFS key from Wallet 3
+      skWallet3 = asymmetricEncryption
+          .generatePrivateKeyFromWalletPrivate(walletCredential3.privateKey);
+
+      // For handling file
+      fileHandler = FileHandler();
     });
 
-    test("Decrypt IPFS key by pinenacl", () {
-      final decrypted =
-          asymmetricEncryption.decryptData(encrypted, skalice, pkbob);
-      expect(message, decrypted);
-    });
-  });
+    group("Test integrity of the keys", () {
+      setUpAll(() {});
+      test("if address are expected from mneumonics", () {
+        expect("0xFE2b19a3545f25420E3a5DAdf11b5582b5B3aBA8",
+            walletCredential1.address);
+        expect("0xAf306F6998FA805797FE60901D3358f18eBA58BC",
+            walletCredential2.address);
+        expect("0xd477b5E16473793909DD84f559e17a794060e644",
+            walletCredential3.address);
+      });
 
-  group("Encrypt Content IPFS", () {
-    String actual = "This is image base 64";
-    String password =
-        "passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword";
-    final fileHandler = FileHandler();
-    String encryptedImage = "";
-    test("Start Encrypt Content", () {
-      final encryptImage = fileHandler.encryption(actual, password);
-      encryptedImage = encryptImage;
-      expect(encryptedImage, encryptImage);
-    });
+      test("Sharing key from wallet 1 to wallet 2", () {
+        String secretMessageFromOnetoTwo = "Secret message from Wallet 1 to 2";
+        String encryptedMessageFromOnetoTwo = asymmetricEncryption.encryptData(
+            secretMessageFromOnetoTwo, skWallet1, skWallet2.publicKey);
 
-    test("Get Content from IPFS", () {
-      final baseImage = fileHandler.decryption(encryptedImage, password);
-      expect(actual, baseImage);
+        print("encryptedMessageFromOnetoTwo $encryptedMessageFromOnetoTwo");
+        String decipherMessageWithWalletOne = asymmetricEncryption.decryptData(
+            encryptedMessageFromOnetoTwo, skWallet2, skWallet1.publicKey);
+
+        expect(decipherMessageWithWalletOne, secretMessageFromOnetoTwo,
+            reason: "Wallet 2 should be able to decipher secret message");
+
+        try {
+          asymmetricEncryption.decryptData(
+              encryptedMessageFromOnetoTwo, skWallet2, skWallet3.publicKey);
+        } catch (e) {
+          expect(e,
+              "The message is forged or malformed or the shared secret is invalid",
+              reason:
+                  "Wallet 3 shouldn't be able to decipher the secret message");
+        }
+      });
+
+      test("Encrypting IPFS file", () {
+        String actual = "This is image base 64";
+        String secretMessageFromOnetoTwo = "Secret message from Wallet 1 to 2";
+
+        // This will be shared / retrieved along the future encrypted IPFS File
+        String encryptedMessageFromOnetoTwo = asymmetricEncryption.encryptData(
+            secretMessageFromOnetoTwo, skWallet1, skWallet2.publicKey);
+
+        fileHandler = FileHandler();
+        final encryptImage =
+            fileHandler.encryption(actual, secretMessageFromOnetoTwo);
+
+        expect(encryptImage.compareTo(actual) != 0, true,
+            reason: "encrypted image and base image should be different now");
+
+        // We can share the 'encryptedMessageFromOnetoTwo' safely, but only the destination public address can decipher
+        String decipherMessageFromOnetoTwo = asymmetricEncryption.decryptData(
+            encryptedMessageFromOnetoTwo, skWallet1, skWallet2.publicKey);
+
+        expect(decipherMessageFromOnetoTwo, secretMessageFromOnetoTwo,
+            reason: "decrypted key and secret message should be the same");
+
+        final baseImage =
+            fileHandler.decryption(encryptImage, decipherMessageFromOnetoTwo);
+        expect(actual, baseImage);
+      });
     });
   });
 }
