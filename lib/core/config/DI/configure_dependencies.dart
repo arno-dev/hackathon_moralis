55a119fbd7c6bdae1977352f862af46089b17e78
Arnaud <arnaud.phommasone@gmail.com>
2022-09-12 23:53:52 +0700

    Initialize firebase

--- lib/core/config/DI/configure_dependencies.dart
+++ lib/core/config/DI/configure_dependencies.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../../firebase_options.dart';
 import 'configure_dependencies.config.dart';
 
 final getIt = GetIt.instance;
@@ -11,5 +13,8 @@ final getIt = GetIt.instance;
 Future<GetIt> configureDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   return $initGetIt(getIt);
 }
