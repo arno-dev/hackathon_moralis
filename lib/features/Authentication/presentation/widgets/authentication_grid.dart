1463ce5efa1da47becf23583995557819047e851
Beer <beer@comet.la>
2022-09-13 19:49:06 +0700

    modified grid botton

--- lib/features/Authentication/presentation/widgets/authentication_grid.dart
+++ lib/features/Authentication/presentation/widgets/authentication_grid.dart
@@ -1,3 +1,4 @@
+import 'package:d_box/core/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'gird_button.dart';
@@ -6,18 +7,23 @@ class AuthenticationGrid extends StatelessWidget {
   const AuthenticationGrid(
       {super.key,
       this.isDisplay = false,
-      required this.borderColor,
+      this.borderColor = Colors.grey,
       required this.gridColor,
-      required this.data});
+      required this.data,
+      this.getIndex,
+      required this.currentIndex});
   final bool isDisplay;
   final Color borderColor;
   final Color gridColor;
   final List<String> data;
+  final int currentIndex;
+  final void Function(int)? getIndex;
+
   @override
   Widget build(BuildContext context) {
-    int rightLength =  ((data.length)/2).round();
-    int leftLength = (data.length)~/2;
-           return Container(
+    int rightLength = ((data.length) / 2).round();
+    int leftLength = (data.length) ~/ 2;
+    return Container(
         padding: const EdgeInsets.fromLTRB(20, 16, 20, 34),
         decoration: BoxDecoration(
             border: Border.all(width: 1, color: gridColor),
@@ -29,15 +35,21 @@ class AuthenticationGrid extends StatelessWidget {
                 children: [
                   Column(
                       children: List.generate(
-                         rightLength,
+                          rightLength,
                           (index) => Column(
                                 children: [
                                   const SizedBox(height: 16),
                                   GridButton(
+                                    onTap: (_) {
+                                      getIndex!(index);
+                                    },
                                     index: index + 1,
-                                    borderColor: borderColor,
+                                    borderColor: currentIndex == index ||
+                                            data[index].isNotEmpty
+                                        ? AppColors.backgroundColor
+                                        : borderColor,
                                     isDottedButton: data[index].isEmpty,
-                                    text: data[index]
+                                    text: data[index],
                                   ),
                                 ],
                               ))),
@@ -48,15 +60,25 @@ class AuthenticationGrid extends StatelessWidget {
                                 children: [
                                   const SizedBox(height: 16),
                                   GridButton(
-                                      index: index+1+ rightLength,
-                                      borderColor: borderColor,
-                                      isDottedButton: data[index + rightLength].isEmpty,text: data[index + rightLength],),
+                                    onTap: (_) {
+                                      getIndex!(index + rightLength);
+                                    },
+                                    index: index + 1 + rightLength,
+                                    borderColor: currentIndex ==
+                                                index + rightLength ||
+                                            data[index + rightLength].isNotEmpty
+                                        ? AppColors.backgroundColor
+                                        : borderColor,
+                                    isDottedButton:
+                                        data[index + rightLength].isEmpty,
+                                    text: data[index + rightLength],
+                                  ),
                                 ],
                               ))),
                 ],
               )
             : Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
@@ -67,7 +89,8 @@ class AuthenticationGrid extends StatelessWidget {
                                   const SizedBox(height: 16),
                                   GridButton(
                                     borderColor: borderColor,
-                                    isDottedButton: data[index].isEmpty,text: data[index],
+                                    isDottedButton: data[index].isEmpty,
+                                    text: data[index],
                                   ),
                                 ],
                               ))),
@@ -78,8 +101,11 @@ class AuthenticationGrid extends StatelessWidget {
                                 children: [
                                   const SizedBox(height: 16),
                                   GridButton(
-                                      borderColor: borderColor,
-                                      isDottedButton: data[index + rightLength].isEmpty,text: data[index + rightLength],),
+                                    borderColor: borderColor,
+                                    isDottedButton:
+                                        data[index + rightLength].isEmpty,
+                                    text: data[index + rightLength],
+                                  ),
                                 ],
                               ))),
                 ],
--- lib/features/Authentication/presentation/widgets/gird_button.dart
+++ lib/features/Authentication/presentation/widgets/gird_button.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/widgets/base_button.dart';
-import '../../../../core/widgets/dotted_button.dart';
+import 'dotted_button.dart';
 
 class GridButton extends StatelessWidget {
   const GridButton(
-      {super.key, this.text,  this.index, this.isDottedButton = true, required this.borderColor, this.onTap});
+      {super.key,
+      this.text,
+      this.index,
+      this.isDottedButton = true,
+      required this.borderColor,
+      this.onTap});
   final String? text;
   final int? index;
   final bool isDottedButton;
-  final Color borderColor; 
+  final Color borderColor;
   final void Function(int)? onTap;
 
   @override
@@ -17,28 +22,34 @@ class GridButton extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-       (index != null)? SizedBox(
-          width: 20,
-          child: Text("$index.",textAlign: TextAlign.end),
-        ) : Container(),
-        const SizedBox(width: 5,),
+        (index != null)
+            ? SizedBox(
+                width: 20,
+                child: Text(
+                  "$index.",
+                  textAlign: TextAlign.end,
+                ),
+              )
+            : Container(),
+        const SizedBox(
+          width: 5,
+        ),
         isDottedButton
-            ?  DottedButton(
-                borderColor:borderColor,
-                onTap: (){
-                  if  ( onTap!=null && index != null)  {
+            ? DottedButton(
+                borderColor: borderColor,
+                onTap: () {
+                  if (onTap != null && index != null) {
                     onTap!(index!);
                   }
-                } 
-              )
+                })
             : BaseButton(
                 text: text,
                 borderColor: borderColor,
-                onTap: (){
-                  if  ( onTap!=null && index != null)  {
+                onTap: () {
+                  if (onTap != null && index != null) {
                     onTap!(index!);
                   }
-                } ,
+                },
               ),
       ],
     );
