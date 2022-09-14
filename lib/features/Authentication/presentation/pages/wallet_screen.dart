0b4eceb701da67f989b34b534e60b173b665f9cb
Along <arnaud.phommasone@gmail.com>
2022-09-14 15:30:41 +0700

    add sizer for project

--- lib/features/Authentication/presentation/pages/wallet_screen.dart
+++ lib/features/Authentication/presentation/pages/wallet_screen.dart
@@ -11,52 +11,53 @@ class WalletPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return Scaffold(
-          backgroundColor: Colors.white,
-          appBar: DAppBar(title: tr('titleCloudmet'),titleStyle: Theme.of(context).textTheme.caption2, leading: false),
-          body: SafeArea(
-              child: SingleChildScrollView(
-            child: Container(
-                margin: EdgeInsets.only(left: 5.w, right: 5.w),
-                child: Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: DAppBar(
+          title: tr('titleCloudmet'),
+          titleStyle: Theme.of(context).textTheme.caption2,
+          leading: false),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(left: 5.w, right: 5.w),
+            child: Column(
+              children: [
+                Image.asset('assets/gifs/addfiles.gif',
+                    gaplessPlayback: true, fit: BoxFit.fill),
+                Column(
                   children: [
-                    Image.asset('assets/gifs/addfiles.gif',
-                        gaplessPlayback: true, fit: BoxFit.fill),
-                    Column(
-                      children: [
-                        Text(tr('welcomeToCloudmet'),
-                            textAlign: TextAlign.center,
-                            style:  Theme.of(context).textTheme.title2),
-                        SizedBox(height: 5.w),
-                        Text(tr('welcomeContent'), textAlign: TextAlign.center)
-                      ],
-                    ),
+                    Text(tr('welcomeToCloudmet'),
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.title2),
                     SizedBox(height: 5.w),
-                    Column(
-                      children: [
-                        BaseButton(
-                            text: tr('importWallet'),
-                            buttonWidth: 100.w,
-                            backgroundColor: AppColors.primaryPurpleColor,
-                            textColor: Colors.white,
-                            buttonHeight: 13.w),
-                        SizedBox(height: 3.w),
-                        BaseButton(
-                          text: tr('createYourWallet'),
-                          buttonWidth: 100.w,
-                          buttonHeight: 13.w,
-                          borderColor: AppColors.primaryPurpleColor,
-                          textColor: AppColors.primaryPurpleColor,
-                        ),
-                      ],
-                    )
+                    Text(tr('welcomeContent'), textAlign: TextAlign.center)
                   ],
-                )),
-          )),
-        );
-      },
+                ),
+                SizedBox(height: 5.w),
+                Column(
+                  children: [
+                    BaseButton(
+                        text: tr('importWallet'),
+                        buttonWidth: 100.w,
+                        backgroundColor: AppColors.primaryPurpleColor,
+                        textColor: Colors.white,
+                        buttonHeight: 13.w),
+                    SizedBox(height: 3.w),
+                    BaseButton(
+                      text: tr('createYourWallet'),
+                      buttonWidth: 100.w,
+                      buttonHeight: 13.w,
+                      borderColor: AppColors.primaryPurpleColor,
+                      textColor: AppColors.primaryPurpleColor,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
